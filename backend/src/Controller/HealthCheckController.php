45ee3c7c41a79e33712ff5e533a02ae659718105
Vladyslav Cherednychenko <vladislav.cherednychenko@gmail.com>
2026-01-19 23:25:27 +0100

    feat: update controllers to use response factory

--- backend/src/Controller/HealthCheckController.php
+++ backend/src/Controller/HealthCheckController.php
@@ -2,33 +2,42 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
+use App\Service\ApiResponseFactory;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/api/health', name: '_api_health_')]
 class HealthCheckController extends AbstractController
 {
+    public function __construct(
+        private TranslatorInterface $translator,
+        private EntityManagerInterface $entityManager,
+        private ApiResponseFactory $responseFactory
+    ) {}
+
     #[Route('', name: 'check', methods: ['GET'])]
-    public function healthCheck(TranslatorInterface $translator, EntityManagerInterface $entityManager): JsonResponse
+    public function healthCheck(): JsonResponse
     {
         $isDbAlive = true;
-        $message = $translator->trans('api.health_check.success');
 
         try {
-            $connection = $entityManager->getConnection();
+            $connection = $this->entityManager->getConnection();
             $connection->executeQuery('SELECT 1');
         } catch (\Exception $e) {
             $isDbAlive = false;
         }
 
-        return new JsonResponse([
-            'status' => 'ok',
-            'message' => $message,
-            'php_version' => PHP_VERSION,
-            'db_available' => $isDbAlive
-        ]);
+        return $this->responseFactory->create(
+            message: $this->translator->trans('api.health_check.response_message'),
+            data: [
+                'status' => 'ok',
+                'message' => $this->translator->trans('api.health_check.success'),
+                'php_version' => PHP_VERSION,
+                'db_available' => $isDbAlive
+            ],
+        );
     }
 }
--- backend/src/Controller/UserController.php
+++ backend/src/Controller/UserController.php
@@ -3,27 +3,28 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Dto\UserRegistrationDto;
+use App\Service\ApiResponseFactory;
 use App\Service\UserServiceInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Component\ObjectMapper\ObjectMapperInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
-use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/api/users', name: '_api_users_')]
 class UserController extends AbstractController
 {
-    private UserServiceInterface $userService;
-    private ValidatorInterface $validator;
-    private SerializerInterface $serializer;
-
-    public function __construct(UserServiceInterface $userService, ValidatorInterface $validator, SerializerInterface $serializer)
-    {
-        $this->userService = $userService;
-        $this->validator = $validator;
-        $this->serializer = $serializer;
-    }
+    public function __construct(
+        private UserServiceInterface $userService,
+        private ValidatorInterface $validator,
+        private ObjectMapperInterface $mapper,
+        private ApiResponseFactory $responseFactory,
+        private TranslatorInterface $translator
+    ) {}
 
     #[Route('', name: 'list', methods: ['GET'])]
     public function list(Request $request): JsonResponse
@@ -32,20 +33,31 @@ class UserController extends AbstractController
         $limit = $request->query->getInt('limit', 10);
 
         if ($page < 1 || $limit < 1) {
-            return $this->json(['error' => 'Pagination parameters must be greater than 0.'], 400);
+            return $this->responseFactory->create(
+                message: $this->translator->trans('api.user_conroller.get_user_list.validation.message'),
+                errors: [
+                    $this->translator->trans('api.user_conroller.get_user_list.validation.errors.pagination')
+                    => $this->translator->trans('api.user_conroller.get_user_list.validation.errors.value'),
+                ],
+                statusCode: 400
+            );
         }
 
         $paginator = $this->userService->getUserList($page, $limit);
-        $totalItems = count($paginator);
+        $totalItems = \count($paginator);
 
-        return $this->json([
-            'data' => $paginator,
-            'meta' => [
-                'total' => $totalItems,
-                'page' => $page,
-                'pages' => ceil($totalItems / $limit)
-            ]
-        ], 200, [], ['groups' => 'user:read']);
+        return $this->responseFactory->create(
+                message: $this->translator->trans('api.user_conroller.get_user_list.result.message'),
+                data: [
+                    'pagination' => $paginator,
+                    'meta' => [
+                        'total' => $totalItems,
+                        'page' => $page,
+                        'pages' => ceil($totalItems / $limit)
+                    ],
+                ],
+                context: ['groups' => 'user:read']
+            );
     }
 
     #[Route('/{id}', name: 'find_by_id', methods: ['GET'])]
@@ -54,33 +66,50 @@ class UserController extends AbstractController
         $user = $this->userService->getUserById($id);
 
         if (!$user) {
-            return $this->json(['message' => 'User not found'], 404);
+            return $this->responseFactory->create($this->translator->trans('api.user_conroller.find_by_id.not_found'), statusCode: 404);
         }
-        return $this->json(['user' => $user], 200, [], ['groups' => 'user:read']);
+        return $this->responseFactory->create(
+            $this->translator->trans('api.user_conroller.find_by_id.found'),
+            ['user' => $user],
+            context: ['groups' => 'user:read']
+        );
     }
 
     #[Route('/register', name: 'register', methods: ['POST'])]
-    public function register(Request $request): JsonResponse {
-        $user = $this->serializer->deserialize($request->getContent(), User::class, 'json');
+    public function register(#[MapRequestPayload] UserRegistrationDto $dto): JsonResponse {
+        $user = $this->mapper->map($dto, User::class);
 
         $errors = $this->validator->validate($user);
-        if (count($errors) > 0) {
+        if (\count($errors) > 0) {
             $messages = [];
             foreach ($errors as $error) {
                 $messages[$error->getPropertyPath()] = $error->getMessage();
             }
-            return $this->json(['errors' => $messages], 400);
+
+            return $this->responseFactory->create(
+                message: $this->translator->trans('api.user_conroller.registration.messages.failed'),
+                errors: $messages,
+                statusCode: 400
+            );
         }
 
         try {
             $newUser = $this->userService->createUser($user);
-
-            return $this->json([
-                'message' => 'User created',
-                'user' => $newUser
-            ], 201, [], ['groups' => 'user:read']);
+            return $this->responseFactory->create(
+                message: $this->translator->trans('api.user_conroller.registration.messages.created'),
+                data: ['user' => $newUser],
+                statusCode: 201,
+                context: ['groups' => 'user:read']
+            );
         } catch (\Exception $e) {
-            return $this->json(['error' => 'Could not create user.'], 409);
+            return $this->responseFactory->create(
+                message: $this->translator->trans('api.user_conroller.registration.messages.failed'),
+                errors: [
+                    $this->translator->trans('api.user_conroller.registration.errors.generic.title')
+                    => $this->translator->trans('api.user_conroller.registration.errors.generic.value')
+                ],
+                statusCode: 409
+            );
         }
     }
 }
