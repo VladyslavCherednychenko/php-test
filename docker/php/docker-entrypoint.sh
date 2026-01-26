68b2463cbd874fc072ad3f78926306e320737543
Vladyslav Cherednychenko <vladislav.cherednychenko@gmail.com>
2026-01-26 16:30:28 +0100

    feat: update docker compose to generate keypair for jwt tokens

--- docker/php/docker-entrypoint.sh
+++ docker/php/docker-entrypoint.sh
@@ -6,9 +6,18 @@ set -e
 mkdir -p var/cache var/log
 chmod -R 777 var/
 
+echo "composer install"
 composer install --no-interaction --optimize-autoloader
 
+if [ ! -f /var/www/html/config/jwt/private.pem ]; then
+  echo "lexik:jwt:generate-keypair"
+  php bin/console lexik:jwt:generate-keypair --no-interaction --overwrite
+fi
+
+echo "doctrine:database:create"
 php bin/console doctrine:database:create --no-interaction --if-not-exists
+
+echo "doctrine:migrations:migrate"
 php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
 
 exec "$@"
